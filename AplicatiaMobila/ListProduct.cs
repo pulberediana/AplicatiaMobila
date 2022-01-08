c360ca531ca9da5c55197fe9695878438d3f9046
DIANA-ROXANA PULBERE <pulbere.diana174128@student.ubbcluj.ro>
2022-01-08 20:05:53 +0200

    Add project files.

---
+++ AplicatiaMobila/ListProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AplicatiaMobila.Models;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace AplicatiaMobila
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
